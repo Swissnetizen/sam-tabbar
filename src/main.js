df2655743b4159f3ae158c9fdff5d19932d9f80e
Samarth AGARWAL <samu.agarwal@gmail.com>
2013-12-27 15:21:05 +0100

    Base

--- src/main.js
+++ src/main.js
@@ -3,21 +3,37 @@
   // Create your component here
   // http://x-tags.org/docs
   
-  xtag.register('x-stub', {
+  xtag.register("sam-tabbar", {
     lifecycle: {
-      created: function() {},
+      created: function() {
+        if (!this.role) {
+          this.role = "tablist";
+        }
+        this.active = this.querySelector("[active]").id;
+      },
       inserted: function() {},
       removed: function() {},
       attributeChanged: function() {}
     }, 
     events: { 
-    
+      "press:delegate([role='tab'])": function (event) {
+        var tabid = event.target.id;
+        if (this.active === tabid) {
+          xtag.fireEvent(this, "activeTapPress", this.active);
+        } else {
+          this.active = tabid;
+          xtag.fireEvent(this, "tabChange", this.active);
+        }
+      }
     },
     accessors: {
-      
+      role: {
+        attribute: true
+      }
+      active: true
     }, 
     methods: {
-      
+    
     }
   });
 
