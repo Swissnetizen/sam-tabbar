05c20e5d3e0d138ec6021a534dfb3e0d848be411
Samarth AGARWAL <samu.agarwal@gmail.com>
2013-12-29 11:43:18 +0100

    Finish component

--- src/main.js
+++ src/main.js
@@ -1,36 +1,39 @@
 (function(){  
-
-  // Create your component here
-  // http://x-tags.org/docs
-  
+  "use strict";
   xtag.register("sam-tabbar", {
     lifecycle: {
       created: function() {
         if (!this.role) {
           this.role = "tablist";
         }
-        this.active = this.querySelector("[active]").id;
       },
-      inserted: function() {},
+      inserted: function() {
+        this.activeTabId = this.querySelector("[data-start-active]").id;
+      },
       removed: function() {},
       attributeChanged: function() {}
     }, 
     events: { 
-      "press:delegate([role='tab'])": function (event) {
-        var tabid = event.target.id;
-        if (this.active === tabid) {
-          xtag.fireEvent(this, "activeTapPress", this.active);
-        } else {
-          this.active = tabid;
-          xtag.fireEvent(this, "tabChange", this.active);
+      "press": function (event) {
+        var el = event.originalTarget,
+            eventName = "activeTabPress";
+        //Checks if a tab was pressed
+        if (!el || el.getAttribute("role") !== "tab") return;
+        var tabid = el.id;
+        //Checks if the active tab wasn't pressed
+        if (this.activeTabId !== tabid) {
+          document.getElementById(this.activeTabId).dataset.active = false;
+          this.activeTabId = tabid;
+          document.getElementById(this.activeTabId).dataset.active = true;
+          eventName = "tabChange";
         }
+        xtag.fireEvent(this, eventName, {detail: this.activeTabId});
       }
     },
     accessors: {
       role: {
-        attribute: true
-      },
-      active: true
+        attribute: {}
+      }
     }, 
     methods: {
     
