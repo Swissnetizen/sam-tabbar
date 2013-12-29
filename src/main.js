912409b93a172be2db1ebe9b833aa6e6422db15f
Samarth AGARWAL <samu.agarwal@gmail.com>
2013-12-29 12:58:44 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,26 +8,17 @@
         }
       },
       inserted: function() {
-        this.activeTabId = this.querySelector("[data-start-active]").id;
+        this.activeTabId = this.querySelector("[role='tab'][data-start-active]").id;
       },
       removed: function() {},
       attributeChanged: function() {}
     }, 
     events: { 
       "press": function (event) {
-        var el = event.originalTarget,
-            eventName = "activeTabPress";
+        var el = event.originalTarget;
         //Checks if a tab was pressed
         if (!el || el.getAttribute("role") !== "tab") return;
-        var tabid = el.id;
-        //Checks if the active tab wasn't pressed
-        if (this.activeTabId !== tabid) {
-          document.getElementById(this.activeTabId).dataset.active = false;
-          this.activeTabId = tabid;
-          document.getElementById(this.activeTabId).dataset.active = true;
-          eventName = "tabChange";
-        }
-        xtag.fireEvent(this, eventName, {detail: this.activeTabId});
+        this.setTab(el.id, true);
       }
     },
     accessors: {
@@ -36,7 +27,23 @@
       }
     }, 
     methods: {
-    
+      setTab: function (tabid, fireEvent) {
+        var eventName = "tabChange";
+        if (!this.querySelector("[id='"+tabid+"'][role='tab']")) {
+          console.error("Cannot set to unknown tabid");
+          return false;
+        }
+        //Checks if person is trying to set to currently active tab
+        if (this.activeTabId === tabid) {
+          eventName = "activeTabPress"
+        } else {
+          document.getElementById(this.activeTabId).dataset.active = false;
+          this.activeTabId = tabid;
+          document.getElementById(this.activeTabId).dataset.active = true;
+        }
+        if (fireEvent) xtag.fireEvent(this, eventName, {detail: this.activeTabId});
+        return true;
+    }
     }
   });
 
